759d56fee7b3b5806eb92294a68f1e83050aba94
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-02-19 21:34:49 -0300

    Create models

--- tikal/core/models.py
+++ tikal/core/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+""" Models """
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    """ Model for customers """
+    user = models.ForeignKey(
+        User, verbose_name='usuário', on_delete=models.CASCADE
+    )
+    webhook_url = models.CharField('url webhook', max_length=255)
+
+    class Meta:
+        """ Class meta """
+        verbose_name = 'customer'
+        verbose_name_plural = 'customers'
+
+    def __str__(self):
+        return self.user
+
+
+class Process(models.Model):
+    """ Model for processes """
+    process_number = models.IntegerField('número do processo', max_length=20)
+    process_data = models.TextField('dados do processo')
+    customer = models.ForeignKey(
+        Customer, verbose_name='cliente', on_delete=models.CASCADE
+    )
+
+    class Meta:
+        """ Class meta """
+        verbose_name = 'customer'
+        verbose_name_plural = 'customers'
+
+    def __str__(self):
+        return self.process_number
