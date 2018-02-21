133e8780cfabf026e6cf2127c1af4ced6fea74ac
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-02-21 13:15:23 -0300

    Changes the meta class of the process model

--- tikal/core/models.py
+++ tikal/core/models.py
@@ -1,7 +1,7 @@
 """ Models """
 
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Customer(models.Model):
@@ -22,7 +22,7 @@ class Customer(models.Model):
 
 class Process(models.Model):
     """ Model for processes """
-    process_number = models.IntegerField('número do processo', max_length=20)
+    process_number = models.IntegerField('número do processo')
     process_data = models.TextField('dados do processo')
     customer = models.ForeignKey(
         Customer, verbose_name='cliente', on_delete=models.CASCADE
@@ -30,8 +30,8 @@ class Process(models.Model):
 
     class Meta:
         """ Class meta """
-        verbose_name = 'customer'
-        verbose_name_plural = 'customers'
+        verbose_name = 'process'
+        verbose_name_plural = 'processes'
 
     def __str__(self):
         return self.process_number
