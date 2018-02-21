83428d7e388fa1919cc7920e81de5ec635ddf96f
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-02-21 19:00:55 -0300

    Add list_process view

--- tikal/core/views.py
+++ tikal/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+""" Views file """
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.serializer import ProcessSerializer
+from core.models import Customer, Process
+
+
+def list_process(request, customer):
+    """ Lists customer processes """
+    context = {}
+    customer = Customer.objects.get(user__username=customer)
+    context['processes'] = Process.objects.filter(customer=customer)
+    context['customer'] = customer
+    return render(request, 'processes.html', context)
