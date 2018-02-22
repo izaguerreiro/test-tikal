3d87ec3e97f77e028473c86e2803a0152483c42c
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-02-22 17:20:25 -0300

    fix static

--- tikal/settings.py
+++ tikal/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'core/static')
   
 # Celery configuration
 CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
