2ab0f3b8b6aa8fc5f1f512b6d6b0c23921803e64
Izabela Guerreiro <izaguerreiro@gmail.com>
2018-02-22 16:59:46 -0300

    Change settings to deploy

--- tikal/settings.py
+++ tikal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@m!uwa-s*)f!ldd@zz@l(#-leec%svjkxref%d+7@@b_ktxt^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
